c871644e13aa699ff26f56606feb9b597541656b
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-10-01 04:30:52 -0600

    adjusted coding on server js file to split that code into different js files to clean it up and make it more readable.  Also allowed for better handling and cleaner processing

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,6 +1,8 @@
-const { builtinModules } = require("module");
 const path = require("path");
 const router = require('express').Router();
+const animalRoutes = require('../apiRoutes/animalRoutes');
+
+router.use(animalRoutes);
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
